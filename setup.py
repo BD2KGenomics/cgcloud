c76ea2726f80509f5eb9d8be9eed3eb4b9a0acb8
Hannes Schmidt <hannes@ucsc.edu>
2013-09-16 14:11:01 -0700

    Added specific version numbers for dependencies

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ setup(
     version="0.1",
     packages=find_packages( ),
     scripts=[ 'cgcloud' ],
-    install_requires=[ 'boto', 'Fabric', 'PyYAML', 'Crypto', 'lxml'],
+    install_requires=[
+        'boto>=2.9.7',
+        'Fabric>=1.7.0',
+        'PyYAML>=3.10',
+        'PyCrypto>=2.6',
+        'lxml>=3.2.1'],
 )
