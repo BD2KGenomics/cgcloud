0822bcc3edc77544513775a52aacee9b3a5f4893
Hannes Schmidt <hannes@ucsc.edu>
2013-11-20 18:50:06 -0800

    Bump cghub-python-lib dependency to latest dev version

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ setup(
     scripts=[ 'cgcloudagent' ],
     install_requires=[
         'cghub-cloud-lib>=1.0.dev1',
-        'cghub-python-lib>=1.2.dev1',
+        'cghub-python-lib>=1.4.dev1',
         'python-daemon>=1.6',
         'boto>=2.9.7',
         'argparse>=1.2.1' if sys.version_info < (2,7) else None
     ],
     dependency_links=[
-        'hg+ssh://hg@bitbucket.org/cghub/cghub-python-lib@default#egg=cghub-python-lib-1.2.dev1',
+        'hg+ssh://hg@bitbucket.org/cghub/cghub-python-lib@default#egg=cghub-python-lib-1.4.dev1',
         'hg+ssh://hg@bitbucket.org/cghub/cghub-cloud-lib@default#egg=cghub-cloud-lib-1.0.dev1'
     ],
     namespace_packages=[
