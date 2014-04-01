049ca4f206a44a6f1ea1f685a04f3ef75cdf11f8
Hannes Schmidt <hannes@ucsc.edu>
2014-03-31 18:28:06 -0700

    Upgrade to latest dev version of cghub-python-lib

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'argparse>=1.2.1' if sys.version_info < (2,7) else None
     ],
     dependency_links=[
-        'hg+ssh://hg@bitbucket.org/cghub/cghub-python-lib@default#egg=cghub-python-lib-1.4.dev1',
+        'hg+ssh://hg@bitbucket.org/cghub/cghub-python-lib@default#egg=cghub-python-lib-1.5.dev1',
         'hg+ssh://hg@bitbucket.org/cghub/cghub-cloud-lib@default#egg=cghub-cloud-lib-1.0.dev1'
     ],
     namespace_packages=[
