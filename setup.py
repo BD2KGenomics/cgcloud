9dcd02a117337e6b1835dcd823507dbdbefd37a2
Hannes Schmidt <hannes@ucsc.edu>
2015-02-17 16:15:26 -0800

    Fixed Github URLs in setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
         'argparse>=1.2.1' if sys.version_info < (2,7) else None
     ],
     dependency_links=[
-        'git+ssh://git@github.com:BD2KGenomics/bd2k-python-lib.git@master#egg=bd2k-python-lib-1.5.dev1',
-        'git+ssh://git@github.com:BD2KGenomics/cgcloud-lib.git@master#egg=cgcloud-lib-1.0.dev1'
+        'git+ssh://git@github.com/BD2KGenomics/bd2k-python-lib.git@master#egg=bd2k-python-lib-1.5.dev1',
+        'git+ssh://git@github.com/BD2KGenomics/cgcloud-lib.git@master#egg=cgcloud-lib-1.0.dev1'
     ],
     namespace_packages=[
         'cgcloud'
