569d58860fe983507150861461d5c3e64212d874
Hannes Schmidt <hannes@ucsc.edu>
2015-03-02 21:07:55 -0800

    Cosmetics

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ setup(
         'boto>=2.16.0',
         'Fabric>=1.7.0',
         'PyYAML>=3.10' ],
-    namespace_packages=[
-        'cgcloud' ],
+    namespace_packages=[ 'cgcloud' ],
     dependency_links=[
         dependency_base_url + 'bd2k-python-lib.git@master#egg=bd2k-python-lib-1.5.dev1',
         dependency_base_url + 'cgcloud-lib.git@master#egg=cgcloud-lib-1.0.dev1' ] )
