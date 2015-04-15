89d91a6cda8b919349a69c28c6f94b0c0b310e4e
Hannes Schmidt <hannes@ucsc.edu>
2015-04-14 19:44:39 -0700

    DRYed up setup.py and added some more project info

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 from setuptools import setup, find_packages
 
-dependency_base_url = 'git+https://github.com/BD2KGenomics/'
+dependency_links = [ ]
+
+
+def add_private_dependency( name, version, git_ref ):
+    url = 'git+https://github.com/BD2KGenomics'
+    dependency_links.append( '{url}/{name}.git@{git_ref}#egg={name}-{version}'.format( locals( ) ) )
+    return "{name}={version}".format( locals( ) )
+
+
+cgcloud_version = '1.0.dev1'
 
 setup(
-    name="cgcloud-bd2k-ci",
-    version="1.0.dev1",
+    name='cgcloud-jenkins',
+    version=cgcloud_version,
+
+    author="Hannes Schmidt",
+    author_email="hannes@ucsc.edu",
+    url='https://github.com/BD2KGenomics/cgcloud-jenkins',
+    description='Setup and manage a Jenkins continuous integration cluster in EC2',
+
     package_dir={ '': 'src' },
     packages=find_packages( 'src' ),
     install_requires=[
-        'cgcloud-lib>=1.0.dev1',
-        'cgcloud-core>=1.0.dev1',
+        add_private_dependency( 'cgcloud-lib', cgcloud_version, 'master' ),
+        add_private_dependency( 'cgcloud-core', cgcloud_version, 'master' ),
         'Fabric>=1.7.0',
         'lxml>=3.2.1'
     ],
@@ -17,7 +32,4 @@ setup(
         'subprocess32'
     ],
     namespace_packages=[ 'cgcloud' ],
-    dependency_links=[
-        dependency_base_url + 'cgcloud-lib.git@master#egg=cgcloud-core-1.0.dev1',
-        dependency_base_url + 'cgcloud-lib.git@master#egg=cgcloud-lib-1.0.dev1'
-    ] )
+    dependency_links=dependency_links )
