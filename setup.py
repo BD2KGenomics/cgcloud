d52cfe79ed5d3583edbf86f2c64a0bd069f01dc4
Hannes Schmidt <hannes@ucsc.edu>
2015-04-14 19:43:09 -0700

    DRYed up setup.py and added some more project info

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
+cgcloud_version = '1.0.dev1'
+
 setup(
-    name="cgcloud-lib",
-    version="1.0.dev1",
+    name='cgcloud-lib',
+    version=cgcloud_version,
+
+    author='Hannes Schmidt',
+    author_email='hannes@ucsc.edu',
+    url='https://github.com/BD2KGenomics/cgcloud-lib',
+    description='Components shared between cgcloud-core and cgcloud-agent',
+
     package_dir={ '': 'src' },
     packages=find_packages( 'src' ),
     install_requires=[
         'boto>=2.36.0'
     ],
     extras_require={
-        'PyCrypto': [ 'PyCrypto>=2.3' ] # otherwise the bundled cgcloud_Crypto will be used
+        'PyCrypto': [ 'PyCrypto>=2.3' ]  # otherwise the bundled cgcloud_Crypto will be used
     },
-    namespace_packages=[ 'cgcloud' ]
-)
+    namespace_packages=[ 'cgcloud' ] )
