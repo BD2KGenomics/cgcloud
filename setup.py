f8e64c054931fa06cb4fed6027c323549a8483a9
Hannes Schmidt <hannes@ucsc.edu>
2015-04-16 17:42:46 -0700

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import sys
 
+from pkg_resources import parse_version
+
 from setuptools import setup, find_packages
 
 dependency_links = [ ]
 
+cgcloud_version = '1.0.dev1'
 
-def add_private_dependency( name, version, git_ref ):
+
+def add_private_dependency( name, version=cgcloud_version, git_ref=None ):
+    if git_ref is None:
+        git_ref = 'master' if parse_version( version ).is_prerelease else version
     url = 'git+https://github.com/BD2KGenomics'
-    dependency_links.append( '{url}/{name}.git@{git_ref}#egg={name}-{version}'.format( locals( ) ) )
-    return "{name}={version}".format( locals( ) )
+    dependency_links.append(
+        '{url}/{name}.git@{git_ref}#egg={name}-{version}'.format( **locals( ) ) )
+    return '{name}=={version}'.format( **locals( ) )
 
-
-cgcloud_version = "1.0.dev1"
 
 setup(
     name='cgcloud-agent',
@@ -28,8 +33,8 @@ setup(
         'console_scripts': [
             'cgcloudagent = cgcloud.agent.ui:main' ], },
     install_requires=filter( None, [
-        add_private_dependency( 'cgcloud-lib', cgcloud_version, 'master ' ),
-        add_private_dependency( 'bd2k-python-lib', '1.5.dev1', 'master' ),
+        add_private_dependency( 'bd2k-python-lib', '1.5' ),
+        add_private_dependency( 'cgcloud-lib' ),
         'python-daemon>=1.6',
         'boto>=2.9.7',
         'argparse>=1.2.1' if sys.version_info < (2, 7) else None ] ),
