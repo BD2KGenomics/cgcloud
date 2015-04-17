8c4c8fa3c7f9fe7f24557225e85385964abf9b69
Hannes Schmidt <hannes@ucsc.edu>
2015-04-16 17:50:54 -0700

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+from pkg_resources import parse_version
 from setuptools import setup, find_packages
 
 dependency_links = [ ]
 
-
-def add_private_dependency( name, version, git_ref ):
-    url = 'git+https://github.com/BD2KGenomics'
-    dependency_links.append( '{url}/{name}.git@{git_ref}#egg={name}-{version}'.format( locals( ) ) )
-    return "{name}={version}".format( locals( ) )
-
-
 cgcloud_version = '1.0.dev1'
+
+
+def add_private_dependency( name, version=cgcloud_version, git_ref=None ):
+    if git_ref is None:
+        git_ref = 'master' if parse_version( version ).is_prerelease else version
+    url = 'git+https://github.com/BD2KGenomics'
+    dependency_links.append(
+        '{url}/{name}.git@{git_ref}#egg={name}-{version}'.format( **locals( ) ) )
+    return '{name}=={version}'.format( **locals( ) )
+
 
 setup(
     name='cgcloud-core',
@@ -19,7 +23,7 @@ setup(
     author_email='hannes@ucsc.edu',
     url='https://github.com/BD2KGenomics/cgcloud-core',
     description='Efficient and reproducible software deployment for EC2 instances',
-    
+
     package_dir={ '': 'src' },
     packages=find_packages( 'src', exclude=[ '*.test' ] ),
     namespace_packages=[ 'cgcloud' ],
@@ -27,8 +31,9 @@ setup(
         'console_scripts': [
             'cgcloud = cgcloud.core.ui:main' ], },
     install_requires=[
-        add_private_dependency( 'bd2k-python-lib', '1.5.dev1', 'master' ),
-        add_private_dependency( 'cgcloud-lib', cgcloud_version, 'master' ),
+        add_private_dependency( 'bd2k-python-lib', '1.5' ),
+        add_private_dependency( 'cgcloud-lib' ),
+        'pkginfo>=1.1'
         'boto>=2.36.0',
         'Fabric>=1.7.0',
         'PyYAML>=3.10' ],
