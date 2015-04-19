316bc1bbb7001a33a87a82b2ddcd9199b876ba50
Hannes Schmidt <hannes@ucsc.edu>
2015-04-19 15:02:15 -0700

    Fix: dependency on cgcloud-lib was missing

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
+from pkg_resources import parse_version
 from setuptools import setup, find_packages
 
 cgcloud_version = '1.0.dev1'
+
+dependency_links = [ ]
+
+
+def add_private_dependency( name, version=cgcloud_version, git_ref=None ):
+    if git_ref is None:
+        git_ref = 'master' if parse_version( version ).is_prerelease else version
+    url = 'git+https://github.com/BD2KGenomics'
+    dependency_links.append(
+        '{url}/{name}.git@{git_ref}#egg={name}-{version}'.format( **locals( ) ) )
+    return '{name}=={version}'.format( **locals( ) )
+
 
 setup(
     name='cgcloud-lib',
@@ -14,6 +27,7 @@ setup(
     package_dir={ '': 'src' },
     packages=find_packages( 'src' ),
     install_requires=[
+        add_private_dependency( 'bd2k-python-lib', '1.5' ),
         'boto>=2.36.0'
     ],
     extras_require={
@@ -21,4 +35,5 @@ setup(
     },
     setup_requires=[
         'nose>=1.3.4' ],
+    dependency_links=dependency_links,
     namespace_packages=[ 'cgcloud' ] )
