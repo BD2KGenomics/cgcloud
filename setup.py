c9bf945fbeda5448e50a562294e3b2112d0d35ba
Hannes Schmidt <hannes@ucsc.edu>
2015-04-30 13:05:57 -0700

    Fix: branch detection fails when setup.py is invoked as "python setup.py ..."

--- setup.py
+++ setup.py
@@ -12,13 +12,14 @@ dependency_links = [ ]
 def add_private_dependency( name, version=cgcloud_version, git_ref=None ):
     if git_ref is None:
         if parse_version( version ).is_prerelease:
+            project_dir = os.path.dirname( os.path.abspath( __file__ ) )
             git_ref = check_output( [ 'git', 'rev-parse', '--abbrev-ref', 'HEAD' ],
-                                    cwd=os.path.dirname( __file__ ) ).strip( )
+                                    cwd=project_dir ).strip( )
             # pip checks out individual commits which creates a detached HEAD, so we look at
             # remote branches containing the
             if git_ref == 'HEAD':
                 git_ref = check_output( [ 'git', 'branch', '-r', '--contains', 'HEAD' ],
-                                        cwd=os.path.dirname( __file__ ) ).strip( )
+                                        cwd=project_dir ).strip( )
                 assert '\n' not in git_ref
                 git_ref = git_ref.split( '/' )
                 assert len( git_ref ) == 2
