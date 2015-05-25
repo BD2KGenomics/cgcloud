c07354e3d4f1970adcd0c82a6ad184d12b2e2211
Hannes Schmidt <hannes@ucsc.edu>
2015-05-25 00:02:22 -0700

    Fixed minor typo

--- mesos-tools/setup.py
+++ mesos-tools/setup.py
@@ -9,7 +9,7 @@ setup(
     author='Christopher Ketchum',
     author_email='cketchum@ucsc.edu',
     url='https://github.com/BD2KGenomics/cgcloud',
-    description='Setup and manage a Apache Mesos cluster in EC2',
+    description='Setup and manage an Apache Mesos cluster in EC2',
 
     package_dir={ '': 'src' },
     packages=find_packages( 'src' ),
