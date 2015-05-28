c21f20528999dc829bdadb04d79303c5356b8e5f
Hannes Schmidt <hannes@ucsc.edu>
2015-05-27 19:07:43 -0700

    each_setup.py: Add support for setup.py commands with spaces in them

--- each_setup.py
+++ each_setup.py
@@ -4,8 +4,10 @@ import os
 import sys
 import subprocess
 
-def green(s):
+
+def green( s ):
     return '\033[92m' + s + '\033[0m'
+
 
 if __name__ == "__main__":
     args = sys.argv[ 1 ]
@@ -13,9 +15,7 @@ if __name__ == "__main__":
     root_path = os.path.dirname( __file__ )
     for project in projects:
         dir_path = os.path.join( root_path, project )
-        print green( "Running 'setup.py %s' in %s" % ( args, dir_path ) )
+        print green( "Attempting to run 'setup.py %s' in %s" % ( args, dir_path ) )
         assert os.path.isdir( dir_path )
         assert os.path.exists( os.path.join( dir_path, 'setup.py' ) )
-        subprocess.check_call(
-            [ sys.executable, "setup.py" ] + args.split(' '),
-            cwd=dir_path )
+        subprocess.check_call( sys.executable + " setup.py " + args, cwd=dir_path, shell=True )
