BD2KGenomics/cgcloud

commit 35fbc543e2aea0a81a345143a91b5306e53c0204

Tests use random namespace for each run & infer zone if running on EC2

core/src/cgcloud/core/test/test_core.py
@@ -1,13 +1,16 @@
+import base64
 import logging
 import os
 from unittest import TestCase
 import itertools
-
-from bd2k.util.exceptions import panic
 from subprocess import check_call
 
-import cgcloud
+from bd2k.util.exceptions import panic
+from boto.utils import get_instance_metadata
+
+from cgcloud.core import BOXES
 from cgcloud.core.ui import main
+from cgcloud.lib.ec2 import running_on_ec2
 
 log = logging.getLogger( __name__ )
 
@@ -18,15 +21,18 @@ class CoreTests( TestCase ):
     """
     _multiprocess_can_split_ = True
 
-    boxes = cgcloud.core.BOXES
+    boxes = BOXES
 
     @classmethod
     def setUpClass( cls ):
         super( CoreTests, cls ).setUpClass( )
-        # FIMXE: use a unique namespace for every run
-        os.environ.setdefault( 'CGCLOUD_NAMESPACE', '/test/' )
-        # FIXME: on EC2 detect zone automatically
-        os.environ.setdefault( 'CGCLOUD_ZONE', 'us-west-2a' )
+        while True:
+            random_suffix = base64.urlsafe_b64encode( os.urandom( 9 ) )
+            if '_' not in random_suffix: break
+        os.environ.setdefault( 'CGCLOUD_NAMESPACE', '/test-%s/' % random_suffix )
+        if running_on_ec2( ):
+            os.environ.setdefault( 'CGCLOUD_ZONE',
+                                   get_instance_metadata( )[ 'placement' ][ 'availability_zone' ] )
 
     @classmethod
     def __box_test( cls, box ):
@@ -59,14 +65,14 @@ class CoreTests( TestCase ):
     def _cgcloud( self, *args ):
         log.info( "Running %r" % (args,) )
         if os.environ.get( 'CGCLOUD_TEST_EXEC', "" ):
-            check_call( ( 'cgcloud', ) + args )
+            check_call( ('cgcloud',) + args )
         else:
             main( args )
 
     ssh_opts = [ '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no' ]
 
     def _ssh( self, role, *args ):
-        self._cgcloud( 'ssh', role, *( itertools.chain( self.ssh_opts, args ) ) )
+        self._cgcloud( 'ssh', role, *(itertools.chain( self.ssh_opts, args )) )
 
     def _rsync( self, role, *args ):
         self._cgcloud( 'rsync', '--ssh-opts=' + ' '.join( self.ssh_opts ), role, *args )

lib/src/cgcloud/lib/ec2.py
@@ -4,6 +4,7 @@ from operator import attrgetter
 import time
 
 from boto.exception import EC2ResponseError
+import errno
 
 from cgcloud.lib.util import UserError
 
@@ -209,3 +210,13 @@ def wait_transition( resource, from_states, to_state, state_getter=attrgetter( '
         state = state_getter( resource )
     if state != to_state:
         raise UnexpectedResourceState( resource, to_state, state )
+
+def running_on_ec2():
+    try:
+        with open( '/sys/hypervisor/uuid' ) as f:
+            return f.read(3) == 'ec2'
+    except IOError as e:
+        if e.errno == errno.ENOENT:
+            return False
+        else:
+            raise

commit 7ff49329f83bacb85a2042554b52a7a9541afd23

Fix: test fail due to presence of wildcard in test namespace arn (regression from 35fbc54)
Use time-base, ordered suffix for test namespace instead of a random one. And extend 35fbc54 to Spark and Mesos tests

core/src/cgcloud/core/__init__.py
@@ -30,3 +30,5 @@ COMMANDS = [
     RegisterKeyCommand,
     CleanupCommand,
     UpdateInstanceProfile ]
+
+test_namespace_suffix_length = 8

jenkins/src/cgcloud/bd2k/ci/cgcloud_jenkins_slave.py
@@ -1,3 +1,4 @@
+from cgcloud.core import test_namespace_suffix_length
 from cgcloud.core.common_iam_policies import ec2_full_policy
 from cgcloud.lib.util import abreviated_snake_case_class_name
 
@@ -30,14 +31,23 @@ class CgcloudJenkinsSlave( UbuntuTrustyGenericJenkinsSlave ):
         role_name, policies = super( CgcloudJenkinsSlave, self )._get_iam_ec2_role( )
         role_name += '--' + abreviated_snake_case_class_name( CgcloudJenkinsSlave )
         cgcloud_bucket_arn = "arn:aws:s3:::%s" % self.ctx.s3_bucket_name
+        # This is a bit convoluted, but it is still better than optionally allowing wildcards in
+        # the name validation in Context.absolute_name(). The ? wildcard is not very well
+        # documented but I found evidence for it here:
+        # http://docs.aws.amazon.com/IAM/latest/UserGuide/PolicyVariables.html#policy-vars-specialchars
+        test_namespace_suffix_pattern = "?" * test_namespace_suffix_length
+        pass_role_arn = self._role_arn( role_prefix='test-testnamespacesuffixpattern/' )
+        pass_role_arn = pass_role_arn.replace( 'testnamespacesuffixpattern',
+                                               test_namespace_suffix_pattern )
         policies.update( dict(
             ec2_full=ec2_full_policy,  # FIXME: Be more specific
             iam_cgcloud_jenkins_slave_pass_role=dict( Version="2012-10-17", Statement=[
-                # This assumes that if instance lives in /, then tests running on the
-                # instance will run in /test. If instance lives in /test, then tests
-                # running on the instance will run in /test/test.
+                # This assumes that if instance lives in /, then tests running on the instance
+                # will run in /test-5571439d. If the instance lives in /foo, then tests running
+                # on the instance will run in /foo/test-5571439d. The suffix 5571439d is just an
+                # example.
                 dict( Effect="Allow",
-                      Resource=self._role_arn( 'test/' ),
+                      Resource=pass_role_arn,
                       Action="iam:PassRole" ) ] ),
             register_keypair=dict( Version="2012-10-17", Statement=[
                 dict( Effect="Allow", Resource="arn:aws:s3:::*", Action="s3:ListAllMyBuckets" ),
@@ -46,7 +56,7 @@ class CgcloudJenkinsSlave( UbuntuTrustyGenericJenkinsSlave ):
                     cgcloud_bucket_arn + "/*" ] ),
                 dict( Effect="Allow",
                       Resource='arn:aws:sns:*:%s:cgcloud-agent-notifications' % self.ctx.account,
-                      Action=[ "sns:Publish", "sns:CreateTopic" ] )] ),
+                      Action=[ "sns:Publish", "sns:CreateTopic" ] ) ] ),
             iam_cgcloud_jenkins_slave=dict( Version="2012-10-17", Statement=[
                 dict( Effect="Allow", Resource="*", Action=[
                     "iam:CreateRole",

core/src/cgcloud/core/test/__init__.py
@@ -1,0 +1,20 @@
+import os
+import time
+from unittest import TestCase
+
+from boto.utils import get_instance_metadata
+
+from cgcloud.core import test_namespace_suffix_length
+from cgcloud.lib.ec2 import running_on_ec2
+
+
+class CgcloudTestCase( TestCase ):
+    @classmethod
+    def setUpClass( cls ):
+        super( CgcloudTestCase, cls ).setUpClass( )
+        suffix = hex( int( time.time( ) ) )[ 2: ]
+        assert len( suffix ) == test_namespace_suffix_length
+        os.environ.setdefault( 'CGCLOUD_NAMESPACE', '/test-%s/' % suffix )
+        if running_on_ec2( ):
+            os.environ.setdefault( 'CGCLOUD_ZONE',
+                                   get_instance_metadata( )[ 'placement' ][ 'availability-zone' ] )

core/src/cgcloud/core/test/test_core.py
@@ -1,38 +1,24 @@
-import base64
 import logging
 import os
-from unittest import TestCase
 import itertools
 from subprocess import check_call
 
 from bd2k.util.exceptions import panic
-from boto.utils import get_instance_metadata
 
 from cgcloud.core import BOXES
+from cgcloud.core.test import CgcloudTestCase
 from cgcloud.core.ui import main
-from cgcloud.lib.ec2 import running_on_ec2
 
 log = logging.getLogger( __name__ )
 
 
-class CoreTests( TestCase ):
+class CoreTests( CgcloudTestCase ):
     """
     Tests the typical life-cycle of instances and images
     """
     _multiprocess_can_split_ = True
 
     boxes = BOXES
-
-    @classmethod
-    def setUpClass( cls ):
-        super( CoreTests, cls ).setUpClass( )
-        while True:
-            random_suffix = base64.urlsafe_b64encode( os.urandom( 9 ) )
-            if '_' not in random_suffix: break
-        os.environ.setdefault( 'CGCLOUD_NAMESPACE', '/test-%s/' % random_suffix )
-        if running_on_ec2( ):
-            os.environ.setdefault( 'CGCLOUD_ZONE',
-                                   get_instance_metadata( )[ 'placement' ][ 'availability-zone' ] )
 
     @classmethod
     def __box_test( cls, box ):

mesos/src/cgcloud/mesos/test/test_mesos.py
@@ -1,13 +1,10 @@
-import os
 import time
 import logging
-import unittest
-from tempfile import mkstemp
 import itertools
 
+from cgcloud.core.test import CgcloudTestCase
 from cgcloud.core.ui import main
 from cgcloud.lib.ec2 import UnexpectedResourceState
-from cgcloud.mesos.mesos_box import heredoc, install_dir
 from cgcloud.mesos import MesosBox, MesosMaster, MesosSlave
 
 log = logging.getLogger( __name__ )
@@ -22,7 +19,7 @@ cleanup =True
 create_image = True
 
 
-class ClusterTests( unittest.TestCase ):
+class ClusterTests( CgcloudTestCase ):
     """
     Tests the typical life-cycle of instances and images
     """
@@ -30,10 +27,6 @@ class ClusterTests( unittest.TestCase ):
     @classmethod
     def setUpClass( cls ):
         super( ClusterTests, cls ).setUpClass( )
-        # FIMXE: use a unique namespace for every run
-        os.environ.setdefault( 'CGCLOUD_NAMESPACE', '/test/' )
-        # FIXME: on EC2 detect zone automatically
-        os.environ.setdefault( 'CGCLOUD_ZONE', 'us-west-2a' )
         if create_image:
             cls._cgcloud( 'create', role, '-I', '-T' )
 

spark/src/cgcloud/spark/test/test_spark.py
@@ -5,6 +5,7 @@ import unittest
 from tempfile import mkstemp
 import itertools
 
+from cgcloud.core.test import CgcloudTestCase
 from cgcloud.core.ui import main
 from cgcloud.spark.spark_box import heredoc, install_dir
 from cgcloud.spark import SparkBox, SparkMaster, SparkSlave
@@ -21,7 +22,7 @@ cleanup = True
 create_image = True
 
 
-class ClusterTests( unittest.TestCase ):
+class ClusterTests( CgcloudTestCase ):
     """
     Tests the typical life-cycle of instances and images
     """
@@ -29,10 +30,6 @@ class ClusterTests( unittest.TestCase ):
     @classmethod
     def setUpClass( cls ):
         super( ClusterTests, cls ).setUpClass( )
-        # FIMXE: use a unique namespace for every run
-        os.environ.setdefault( 'CGCLOUD_NAMESPACE', '/test/' )
-        # FIXME: on EC2 detect zone automatically
-        os.environ.setdefault( 'CGCLOUD_ZONE', 'us-west-2a' )
         if create_image:
             cls._cgcloud( 'create', role, '-I', '-T' )
 