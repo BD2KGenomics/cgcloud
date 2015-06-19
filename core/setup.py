004dd8218b6834a1bfd3325835aa698429b3e6c3
Hannes Schmidt <hannes@ucsc.edu>
2015-06-18 19:02:39 -0700

    Prepare next development cycle

--- core/setup.py
+++ core/setup.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 from pkg_resources import parse_version
 from setuptools import setup, find_packages
 
-cgcloud_version = '1.0.dev7'
+cgcloud_version = '1.0.dev8'
 
 setup(
     name='cgcloud-core',
--- lib/setup.py
+++ lib/setup.py
@@ -2,7 +2,7 @@ import os
 from pkg_resources import parse_version
 from setuptools import setup, find_packages
 
-cgcloud_version = '1.0.dev7'
+cgcloud_version = '1.0.dev8'
 
 setup(
     name='cgcloud-lib',
