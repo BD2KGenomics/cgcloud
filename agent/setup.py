ee4f184600797587aa8a752a1faf2729f13640fe
Hannes Schmidt <hannes@ucsc.edu>
2015-07-30 22:23:38 -0700

    Fix: direct dependency of agent on bd2k-python-lib is versioned inconsistently with the transitive one through lib
    Don't know why pip silently violates the ==1.6.dev1 ref by installing 1.7.dev1, even though it could have satisfied both by using 1.6.dev1 which matches ==1.6.dev1 and >=1.6.dev1.
    
    [52.25.171.223] run: /opt/cgcloudagent/bin/cgcloudagent --init-script --zone us-west-2a --namespace /hannes/ --accounts ubuntu --keypairs hannes@ucsc.edu @@developers --user root --group root --pid-file /var/run/cgcloudagent/cgcloudagent.pid --log-spill /var/log/cgcloudagent.out| gzip -c | base64
    [52.25.171.223] out: Traceback (most recent call last):
    [52.25.171.223] out:   File "/opt/cgcloudagent/bin/cgcloudagent", line 5, in <module>
    [52.25.171.223] out:     from pkg_resources import load_entry_point
    [52.25.171.223] out:   File "/opt/cgcloudagent/local/lib/python2.7/site-packages/pkg_resources/__init__.py", line 3084, in <module>
    [52.25.171.223] out:     @_call_aside
    [52.25.171.223] out:   File "/opt/cgcloudagent/local/lib/python2.7/site-packages/pkg_resources/__init__.py", line 3070, in _call_aside
    [52.25.171.223] out:     f(*args, **kwargs)
    [52.25.171.223] out:   File "/opt/cgcloudagent/local/lib/python2.7/site-packages/pkg_resources/__init__.py", line 3097, in _initialize_master_working_set
    [52.25.171.223] out:     working_set = WorkingSet._build_master()
    [52.25.171.223] out:   File "/opt/cgcloudagent/local/lib/python2.7/site-packages/pkg_resources/__init__.py", line 653, in _build_master
    [52.25.171.223] out:     return cls._build_from_requirements(__requires__)
    [52.25.171.223] out:   File "/opt/cgcloudagent/local/lib/python2.7/site-packages/pkg_resources/__init__.py", line 666, in _build_from_requirements
    [52.25.171.223] out:     dists = ws.resolve(reqs, Environment())
    [52.25.171.223] out:   File "/opt/cgcloudagent/local/lib/python2.7/site-packages/pkg_resources/__init__.py", line 839, in resolve
    [52.25.171.223] out:     raise DistributionNotFound(req, requirers)
    [52.25.171.223] out: pkg_resources.DistributionNotFound: The 'bd2k-python-lib==1.6.dev1' distribution was not found and is required by cgcloud-agent
    [52.25.171.223] out: H4sIAGH+ulUAAwMAAAAAAAAAAAA=

--- agent/setup.py
+++ agent/setup.py
@@ -22,7 +22,7 @@ setup(
         'console_scripts': [
             'cgcloudagent = cgcloud.agent.ui:main' ], },
     install_requires=filter( None, [
-        'bd2k-python-lib==1.6.dev1',
+        'bd2k-python-lib>=1.6.dev1',
         'cgcloud-lib==' + cgcloud_version,
         'boto>=2.36.0',
         'python-daemon>=2.0.5',
