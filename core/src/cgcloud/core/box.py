BD2KGenomics/cgcloud

commit 8ebb505817d9d1dfcff5c66f6fbaa8505974bbba

Fix: on jenkins slaves, agent doesn't update ~jenkins/.ssh/authorized_keys file

jenkins/src/cgcloud/bd2k/ci/jenkins_slave.py
@@ -1,4 +1,5 @@
 from lxml.builder import E
+from cgcloud.core.agent_box import AgentBox
 
 from cgcloud.lib.util import snake_to_camel, UserError
 from cgcloud.fabric.operations import sudo
@@ -9,11 +10,14 @@ from cgcloud.bd2k.ci.jenkins_master import Jenkins, JenkinsMaster
 build_dir = '/home/jenkins/builds'
 
 
-class JenkinsSlave( SourceControlClient ):
+class JenkinsSlave( SourceControlClient, AgentBox ):
     """
     A box that represents EC2 instances which can serve as a Jenkins build agent. This class is
     typically used as a mix-in.
     """
+
+    def other_accounts( self ):
+        return super( JenkinsSlave, self ).other_accounts( ) + [ Jenkins.user ]
 
     def _post_install_packages( self ):
         super( JenkinsSlave, self )._post_install_packages( )

commit bcb71679eb40c6090c9ee55c01ba118c813a93b6

Fix: Jenkins user can't log in as jenkins on Jenkins slaves (regression from 8ebb505817d9d1dfcff5c66f6fbaa8505974bbba)

core/src/cgcloud/core/box.py
@@ -144,15 +144,11 @@ class Box( object ):
     def _populate_ec2_keypair_globs( self, ec2_keypair_globs ):
         """
         Populate the given list with keypair globs defining the set of keypairs whose public
-        component will be deployed to this box. The base implementation simply returns the
-        argument.
+        component will be deployed to this box.
 
-        :param ec2_keypair_globs: the suggested list of globs, may be modified in place
-
-        :return: the actual list of globs to be used on this box, may be the argument, or a new
-        list
+        :param ec2_keypair_globs: the suggested list of globs, to be modified in place
         """
-        return ec2_keypair_globs
+        pass
 
     def __init__( self, ctx ):
         """
@@ -361,7 +357,7 @@ class Box( object ):
         self._set_instance_options( str_options )
         self.image_id = image.id
 
-        ec2_keypair_globs = self._populate_ec2_keypair_globs( ec2_keypair_globs )
+        self._populate_ec2_keypair_globs( ec2_keypair_globs )
         ec2_keypairs = self.ctx.expand_keypair_globs( ec2_keypair_globs )
         if not ec2_keypairs:
             raise UserError( "No key pairs matching '%s' found." % ' '.join( ec2_keypair_globs ) )

jenkins/src/cgcloud/bd2k/ci/jenkins_slave.py
@@ -33,6 +33,10 @@ class JenkinsSlave( SourceControlClient, AgentBox ):
                              "creating slaves." % ec2_keypair_name )
         return self.ctx.download_ssh_pubkey( ec2_keypair )
 
+    def _populate_ec2_keypair_globs( self, ec2_keypair_globs ):
+        super( JenkinsSlave, self )._populate_ec2_keypair_globs( ec2_keypair_globs )
+        ec2_keypair_globs.append( JenkinsMaster.ec2_keypair_name( self.ctx ) )
+
     @fabric_task
     def _setup_build_user( self ):
         """
@@ -50,7 +54,7 @@ class JenkinsSlave( SourceControlClient, AgentBox ):
         sudo( 'useradd -m -s /bin/bash {0}'.format( Jenkins.user ) )
         self._propagate_authorized_keys( Jenkins.user )
 
-        # Ensure that jenkins@build-master can log into this box as the build user
+        # Ensure that jenkins@jenkins-master can log into this box as the build user
         #
         sudo( "echo '{pubkey}' >> ~/.ssh/authorized_keys".format( **kwargs ),
               user=Jenkins.user,