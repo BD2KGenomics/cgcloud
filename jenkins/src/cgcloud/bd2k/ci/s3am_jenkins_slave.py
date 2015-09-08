590dee28903a44ff589630ace27c064c19b92ea9
Hannes Schmidt <hannes@ucsc.edu>
2015-09-07 18:29:03 -0700

    Patch Python's asynchat on s3am-jenkins-slave; fixes S3AM test failures
    http://bugs.python.org/issue16133

--- jenkins/src/cgcloud/bd2k/ci/s3am_jenkins_slave.py
+++ jenkins/src/cgcloud/bd2k/ci/s3am_jenkins_slave.py
@@ -1,6 +1,8 @@
 from cgcloud.bd2k.ci.generic_jenkins_slaves import UbuntuTrustyGenericJenkinsSlave
+from cgcloud.core.box import fabric_task
 from cgcloud.core.common_iam_policies import s3_full_policy
-from cgcloud.lib.util import abreviated_snake_case_class_name
+from cgcloud.fabric.operations import remote_sudo_popen
+from cgcloud.lib.util import abreviated_snake_case_class_name, heredoc
 
 
 class S3amJenkinsSlave( UbuntuTrustyGenericJenkinsSlave ):
@@ -15,11 +17,60 @@ class S3amJenkinsSlave( UbuntuTrustyGenericJenkinsSlave ):
     def _list_packages_to_install( self ):
         return super( S3amJenkinsSlave, self )._list_packages_to_install( ) + [
             'python-dev',
-            'gcc', 'make', 'libcurl4-openssl-dev' # pycurl
+            'gcc', 'make', 'libcurl4-openssl-dev'  # pycurl
         ]
+
+    def _post_install_packages( self ):
+        super( S3amJenkinsSlave, self )._post_install_packages( )
+        self.__patch_asynchat( )
 
     def _get_iam_ec2_role( self ):
         role_name, policies = super( S3amJenkinsSlave, self )._get_iam_ec2_role( )
         role_name += '--' + abreviated_snake_case_class_name( S3amJenkinsSlave )
         policies.update( dict( s3_full=s3_full_policy ) )
         return role_name, policies
+
+    @fabric_task
+    def __patch_asynchat( self ):
+        """
+        This bites us in pyftpdlib during S3AM unit tests:
+
+        http://jenkins.cgcloud.info/job/s3am/13/testReport/junit/src.s3am.test.s3am_tests/CoreTests/test_copy/
+
+        The patch is from
+
+        https://hg.python.org/cpython/rev/d422062d7d36
+        http://bugs.python.org/issue16133
+        """
+        with remote_sudo_popen( 'patch -d /usr/lib/python2.7 -p2' ) as patch:
+            patch.write( heredoc( '''
+                diff --git a/Lib/asynchat.py b/Lib/asynchat.py
+                --- a/Lib/asynchat.py
+                +++ b/Lib/asynchat.py
+                @@ -46,12 +46,17 @@ method) up to the terminator, and then c
+                 you - by calling your self.found_terminator() method.
+                 """
+
+                +import asyncore
+                +import errno
+                 import socket
+                -import asyncore
+                 from collections import deque
+                 from sys import py3kwarning
+                 from warnings import filterwarnings, catch_warnings
+
+                +_BLOCKING_IO_ERRORS = (errno.EAGAIN, errno.EALREADY, errno.EINPROGRESS,
+                +                       errno.EWOULDBLOCK)
+                +
+                +
+                 class async_chat (asyncore.dispatcher):
+                     """This is an abstract class.  You must derive from this class, and add
+                     the two methods collect_incoming_data() and found_terminator()"""
+                @@ -109,6 +114,8 @@ class async_chat (asyncore.dispatcher):
+                         try:
+                             data = self.recv (self.ac_in_buffer_size)
+                         except socket.error, why:
+                +            if why.args[0] in _BLOCKING_IO_ERRORS:
+                +                return
+                             self.handle_error()
+                             return''' ) )
