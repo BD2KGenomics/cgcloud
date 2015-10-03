10bd40db68b95994507a9122b4e5872e274f73f2
Hannes Schmidt <hannes@ucsc.edu>
2015-10-03 03:18:59 -0700

    Clean up Mesos and Toil (resolves https://github.com/BD2KGenomics/toil/issues/432)
    - use SSH host keys for connections to the master
    - fixed permissions problem of shared dir

--- toil/src/cgcloud/toil/toil_box.py
+++ toil/src/cgcloud/toil/toil_box.py
@@ -1,47 +1,30 @@
 import logging
-from textwrap import dedent
-
-from bd2k.util.strings import interpolate as fmt
 
 from cgcloud.core.box import fabric_task
-from cgcloud.mesos.mesos_box import MesosBox, MesosMaster, MesosSlave
+from cgcloud.mesos.mesos_box import MesosBox, MesosMaster, MesosSlave, user
 from cgcloud.fabric.operations import sudo
 from cgcloud.lib.util import abreviated_snake_case_class_name
 from cgcloud.core.common_iam_policies import ec2_full_policy, s3_full_policy, sdb_full_policy
 
 log = logging.getLogger( __name__ )
 
-mesos_version = '0.22'
 
-user = 'mesosbox'
-
-install_dir = '/opt/mesosbox'
-
-log_dir = '/var/log/mesosbox/'
-
-ephemeral_dir = '/mnt/ephemeral'
-
-persistent_dir = '/mnt/persistent'
-
-class ToilBox(MesosBox):
-    def __init__( self, ctx):
-        super( ToilBox, self ).__init__( ctx)
-        self.lazy_dirs=set()
-
-    def _pre_install_packages( self ):
-        super( ToilBox, self)._pre_install_packages()
+class ToilBox( MesosBox ):
+    def __init__( self, ctx ):
+        super( ToilBox, self ).__init__( ctx )
+        self.lazy_dirs = set( )
 
     def _post_install_packages( self ):
         super( ToilBox, self )._post_install_packages( )
-        self._upgrade_pip( )
+        self.__upgrade_pip( )
         self.__install_toil( )
-        self.__install_s3am()
-        self._docker_group(user=user)
+        self.__install_s3am( )
+        self._docker_group( user=user )
 
     def _list_packages_to_install( self ):
         return super( ToilBox, self )._list_packages_to_install( ) + [
-            'python-dev','docker.io', 'gcc', 'make',
-            'libcurl4-openssl-dev'] # Only for S3AM
+            'python-dev', 'docker.io', 'gcc', 'make',
+            'libcurl4-openssl-dev' ]  # Only for S3AM
 
     def _get_iam_ec2_role( self ):
         role_name, policies = super( ToilBox, self )._get_iam_ec2_role( )
@@ -56,42 +39,43 @@ class ToilBox(MesosBox):
                 dict( Effect="Allow", Resource="*", Action="ec2:AttachVolume" ) ] ) ) )
         return role_name, policies
 
-    def _mount_mesos_workdir(self):
-        super(ToilBox, self)._mount_mesos_workdir()
-        self._lazy_mkdir('/var/lib','toil', persistent=True)
+    def _setup_mesos( self ):
+        super( ToilBox, self )._setup_mesos( )
+        self._lazy_mkdir( '/var/lib', 'toil', persistent=True )
 
     @fabric_task
-    def __install_s3am(self):
-        sudo("pip install --pre s3am", pty=False)
+    def __install_s3am( self ):
+        sudo( "pip install --pre s3am", pty=False )
 
     @fabric_task
-    def _docker_group(self, user=user):
-        sudo("gpasswd -a {} docker".format(user))
-        sudo("service docker.io restart")
+    def _docker_group( self, user=user ):
+        sudo( "gpasswd -a {} docker".format( user ) )
+        sudo( "service docker.io restart" )
 
     @fabric_task
-    def _upgrade_pip(self):
-        # old version of pip doesn't seem to recognize toil's 'extra' syntax
-        sudo("pip install --upgrade pip", pty=False)
+    def __upgrade_pip( self ):
+        # Older versions of pip don't support the 'extra' mechanism used by Toil's setup.py
+        sudo( "pip install --upgrade pip", pty=False )
 
     @fabric_task
-    def __install_toil(self):
-        sudo("pip install toil[aws,mesos]", pty=False)
+    def __install_toil( self ):
+        sudo( "pip install toil[aws,mesos]", pty=False )
 
-class ToilLeader(ToilBox, MesosMaster):
-    def __init__( self, ctx):
-        super( ToilLeader, self ).__init__( ctx)
+
+class ToilLeader( ToilBox, MesosMaster ):
+    def __init__( self, ctx ):
+        super( ToilLeader, self ).__init__( ctx )
         pass
 
     def _post_install_packages( self ):
         super( ToilLeader, self )._post_install_packages( )
 
-    def clone( self, num_slaves, slave_instance_type, ebs_volume_size):
+    def clone( self, num_slaves, slave_instance_type, ebs_volume_size ):
         """
         Create a number of slave boxes that are connected to this master.
         """
         master = self
-        first_slave = ToilWorker( master.ctx, num_slaves, master.instance_id, ebs_volume_size)
+        first_slave = ToilWorker( master.ctx, num_slaves, master.instance_id, ebs_volume_size )
         args = master.preparation_args
         kwargs = master.preparation_kwargs.copy( )
         kwargs[ 'instance_type' ] = slave_instance_type
@@ -101,14 +85,10 @@ class ToilLeader(ToilBox, MesosMaster):
         all_slaves = [ first_slave ] + other_slaves
         return all_slaves
 
-class ToilWorker(ToilBox, MesosSlave):
-    def __init__( self, ctx, num_slaves=1, mesos_master_id=None, ebs_volume_size=0):
-        super( ToilWorker, self ).__init__( ctx)
+
+class ToilWorker( ToilBox, MesosSlave ):
+    def __init__( self, ctx, num_slaves=1, mesos_master_id=None, ebs_volume_size=0 ):
+        super( ToilWorker, self ).__init__( ctx )
         self.num_slaves = num_slaves
         self.mesos_master_id = mesos_master_id
         self.ebs_volume_size = ebs_volume_size
-
-def heredoc( s ):
-    if s[ 0 ] == '\n': s = s[ 1: ]
-    if s[ -1 ] != '\n': s += '\n'
-    return fmt( dedent( s ), skip_frames=1 )
