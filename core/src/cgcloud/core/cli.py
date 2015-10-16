BD2KGenomics/cgcloud

commit 1008d4a483b09ebc17d6e9a987b0168397414985

Improve error messages when plugin can't be loaded (resolves #71)

core/src/cgcloud/core/cli.py
@@ -1,30 +1,54 @@
 # PYTHON_ARGCOMPLETE_OK
 
+from __future__ import absolute_import
 from collections import OrderedDict
 from importlib import import_module
 import logging
 import os
 import sys
 import imp
+from bd2k.util.iterables import cons
 
-from cgcloud.lib.util import Application, app_name
+from cgcloud.lib.util import Application, app_name, UserError
 import cgcloud.core
 
 log = logging.getLogger( __name__ )
 
 
+def plugin_module( plugin ):
+    """
+    >>> plugin_module('cgcloud.core') # doctest: +ELLIPSIS
+    <module 'cgcloud.core' from '...'>
+    >>> plugin_module('cgcloud.foobar')
+    Traceback (most recent call last):
+    ...
+    UserError: Cannot find plugin module 'cgcloud.foobar'. Running 'pip install cgcloud-foobar' may fix this.
+    """
+    try:
+        return import_module( plugin )
+    except ImportError:
+        raise UserError(
+            "Cannot find plugin module '%s'. Running 'pip install %s' may fix this." % (
+                plugin, plugin.replace( '.', '-' )) )
+
+
 def main( args=None ):
     """
-    This is the cgcloud entrypoint. It should be installed via setuptools.setup( entry_points=... )
+    This is the cgcloud entry point. It should be installed via setuptools.setup(entry_points=...)
     """
-    plugins = [ cgcloud.core ] + [ import_module( plugin )
-        for plugin in os.environ.get( 'CGCLOUD_PLUGINS', "" ).split( ":" )
-        if plugin ]
-    app = CGCloud( plugins )
-    for plugin in plugins:
-        for command_class in plugin.command_classes( ):
-            app.add( command_class )
-    app.run( args )
+    logging.basicConfig( )
+    try:
+        plugins = os.environ.get( 'CGCLOUD_PLUGINS', '' ).strip( )
+        plugins = cons( cgcloud.core,
+                        (plugin_module( plugin ) for plugin in plugins.split( ":" ) if plugin) )
+        app = CGCloud( plugins )
+        for plugin in plugins:
+            for command_class in plugin.command_classes( ):
+                app.add( command_class )
+        app.run( args )
+    except UserError as e:
+        log.error( e.message )
+        sys.exit( 255 )
 
 
 class CGCloud( Application ):

lib/src/cgcloud/lib/util.py
@@ -287,11 +287,7 @@ class Application( object ):
         options = self.parser.parse_args( args )
         self.prepare( options )
         command = self.commands[ options.command_name ]
-        try:
-            command.run( options )
-        except UserError as e:
-            log.error( e.message )
-            sys.exit( 255 )
+        command.run( options )
 
     def prepare( self, options ):
         pass

commit b2aba9fa48e860e22f87b01abcb92a9c40048540

FIx: logging setup wrong (regression from 1008d4a483b09ebc17d6e9a987b0168397414985)

core/src/cgcloud/core/cli.py
@@ -36,12 +36,12 @@ def main( args=None ):
     """
     This is the cgcloud entry point. It should be installed via setuptools.setup(entry_points=...)
     """
-    logging.basicConfig( )
+    root_logger = setup_logging( )
     try:
         plugins = os.environ.get( 'CGCLOUD_PLUGINS', '' ).strip( )
         plugins = cons( cgcloud.core,
                         (plugin_module( plugin ) for plugin in plugins.split( ":" ) if plugin) )
-        app = CGCloud( plugins )
+        app = CGCloud( plugins, root_logger )
         for plugin in plugins:
             for command_class in plugin.command_classes( ):
                 app.add( command_class )
@@ -51,14 +51,29 @@ def main( args=None ):
         sys.exit( 255 )
 
 
+def setup_logging( ):
+    root_logger = logging.getLogger( )
+    # Only setup logging if it hasn't been done already
+    if len( root_logger.handlers ) == 0:
+        root_logger.setLevel( logging.INFO )
+        stream_handler = logging.StreamHandler( sys.stderr )
+        stream_handler.setFormatter( logging.Formatter( "%(levelname)s: %(message)s" ) )
+        stream_handler.setLevel( logging.INFO )
+        root_logger.addHandler( stream_handler )
+        return root_logger
+    else:
+        return None
+
+
 class CGCloud( Application ):
     """
     The main CLI application
     """
     debug_log_file_name = '%s.{pid}.log' % app_name( )
 
-    def __init__( self, plugins ):
+    def __init__( self, plugins, root_logger=None ):
         super( CGCloud, self ).__init__( )
+        self.root_logger = root_logger
         self.option( '--debug',
                      default=False, action='store_true',
                      help='Write debug log to %s in current directory.' % self.debug_log_file_name )
@@ -73,21 +88,15 @@ class CGCloud( Application ):
             self.roles[ role.role( ) ] = role
 
     def prepare( self, options ):
-        root_logger = logging.getLogger( )
-        if len( root_logger.handlers ) == 0:
-            root_logger.setLevel( logging.INFO )
-            stream_handler = logging.StreamHandler( sys.stderr )
-            stream_handler.setFormatter( logging.Formatter( "%(levelname)s: %(message)s" ) )
-            stream_handler.setLevel( logging.INFO )
-            root_logger.addHandler( stream_handler )
+        if self.root_logger:
             if options.debug:
-                root_logger.setLevel( logging.DEBUG )
+                self.root_logger.setLevel( logging.DEBUG )
                 file_name = self.debug_log_file_name.format( pid=os.getpid( ) )
                 file_handler = logging.FileHandler( file_name )
                 file_handler.setLevel( logging.DEBUG )
                 file_handler.setFormatter( logging.Formatter(
                     '%(asctime)s: %(levelname)s: %(name)s: %(message)s' ) )
-                root_logger.addHandler( file_handler )
+                self.root_logger.addHandler( file_handler )
             else:
                 # There are quite a few cases where we expect AWS requests to fail, but it seems
                 # that boto handles these by logging the error *and* raising an exception. We