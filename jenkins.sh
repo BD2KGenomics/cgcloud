683e590bc07333495b9ccd5f92772f2c9d2a30a7
Hannes Schmidt <hannes@ucsc.edu>
2015-12-16 10:13:29 -0800

    Testing parallel build with make -j

--- jenkins.sh
+++ jenkins.sh
@@ -12,4 +12,4 @@ export CGCLOUD_ME=jenkins@jenkins-master
 # which the tests fail and keeps testing the other projects. Unfortunately, that takes away the
 # convenience of specifiying multiple targets in one make invocation since make would not stop on a
 # failing target.
-for target in $make_targets; do make --keep-going $target; done
+for target in $make_targets; do make -j 8 --keep-going $target; done
