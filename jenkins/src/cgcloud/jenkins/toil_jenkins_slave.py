2b6799fa6d25794e066ddd0df9a2451563a5b13f
Arjun Arkal Rao <aarao@ucsc.edu>
2016-03-01 16:26:16 -0800

    toil-jenkins-slave: Add mesos credentials (resolves #114)
    resolves #114
    
    Some future Toil Mesos tests needs a mesos credential file. Credentials are set up at
    /var/lib/mesos/credentials.txt and should persist if an image is created from the box as
    /var/lib/mesos is on the root volume.

--- jenkins/src/cgcloud/jenkins/toil_jenkins_slave.py
+++ jenkins/src/cgcloud/jenkins/toil_jenkins_slave.py
@@ -48,7 +48,15 @@ class ToilJenkinsSlave( UbuntuTrustyGenericJenkinsSlave,
         self.__install_parasol( )
         self.__patch_distutils( )
         self.__configure_gridengine( )
+        self.__createMesosCredentialsFile( )
 
+    @fabric_task
+    def __createMesosCredentialsFile(self):
+        # Create the credentials file and transfer ownership to mesosbox
+        sudo( 'echo toil liot > /var/lib/mesos/credentials.txt' )
+        sudo( 'chown jenkins:jenkins /var/lib/mesos/credentials.txt')
+        sudo( 'chmod 755 /var/lib/mesos/credentials.txt')
+        
     @fabric_task
     def __disable_mesos_daemons( self ):
         for daemon in ('master', 'slave'):
