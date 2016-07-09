03d68b0f5fcf8a52c8c4390369de25567f6fe755
Hannes Schmidt <hannes@ucsc.edu>
2016-07-08 22:20:44 -0700

    Bump Toil to 3.3.0 (#201)

--- toil/src/cgcloud/toil/toil_box.py
+++ toil/src/cgcloud/toil/toil_box.py
@@ -124,7 +124,7 @@ class ToilBox( ToilBoxSupport ):
     A box with Mesos, the latest stable Toil release and their dependencies installed.
     """
 
-    default_spec = 'toil[aws,mesos,encryption,cwl]==3.2.1'
+    default_spec = 'toil[aws,mesos,encryption,cwl]==3.3.0'
 
     @classmethod
     def get_role_options( cls ):
@@ -176,7 +176,7 @@ class ToilLatestBox( ToilBox ):
     """
     A box with Mesos, the latest unstable release of Toil and their dependencies installed
     """
-    default_spec = 'toil[aws,mesos,encryption,cwl]<=3.3.0'
+    default_spec = 'toil[aws,mesos,encryption,cwl]<=3.4.0'
 
 
 class ToilLeader( ToilBox, ClusterLeader ):
