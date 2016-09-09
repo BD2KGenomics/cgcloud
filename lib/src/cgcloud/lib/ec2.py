295a28249d96033c9460cde3c5076a97066cbb2b
Hannes Schmidt <hannes@ucsc.edu>
2016-09-08 23:47:50 -0700

    Reimplement `retry_ec2` using `retry` from bd2k-python-lib

--- lib/src/cgcloud/lib/ec2.py
+++ lib/src/cgcloud/lib/ec2.py
@@ -2,13 +2,13 @@ import errno
 import logging
 import time
 from collections import Iterator
-from contextlib import contextmanager
 from operator import attrgetter
 
 from bd2k.util.exceptions import panic
-from boto.ec2.spotinstancerequest import SpotInstanceRequest
-from boto.ec2.instance import Instance
+from bd2k.util.retry import retry
 from boto.ec2.ec2object import TaggedEC2Object
+from boto.ec2.instance import Instance
+from boto.ec2.spotinstancerequest import SpotInstanceRequest
 from boto.exception import EC2ResponseError
 
 from cgcloud.lib.util import UserError
@@ -24,103 +24,9 @@ def not_found( e ):
     return e.error_code.endswith( '.NotFound' )
 
 
-def true( _ ):
-    return True
-
-
-def false( _ ):
-    return False
-
-
-def retry_ec2( retry_after=a_short_time,
-               retry_for=10 * a_short_time,
-               retry_while=not_found ):
-    """
-    Retry an EC2 operation while the failure matches a given predicate and until a given timeout
-    expires, waiting a given amount of time in between attempts. This function is a generator
-    that yields contextmanagers. See doctests below for example usage.
-
-    :param retry_after: the delay in seconds between attempts
-
-    :param retry_for: the timeout in seconds.
-
-    :param retry_while: a callable with one argument, an instance of EC2ResponseError, returning
-    True if another attempt should be made or False otherwise
-
-    :return: a generator yielding contextmanagers
-
-    Retry for a limited amount of time:
-    >>> i = 0
-    >>> for attempt in retry_ec2( retry_after=0, retry_for=.1, retry_while=true ):
-    ...     with attempt:
-    ...         i += 1
-    ...         raise EC2ResponseError( 'foo', 'bar' )
-    Traceback (most recent call last):
-    ...
-    EC2ResponseError: EC2ResponseError: foo bar
-    <BLANKLINE>
-    >>> i > 1
-    True
-
-    Do exactly one attempt:
-    >>> i = 0
-    >>> for attempt in retry_ec2( retry_for=0 ):
-    ...     with attempt:
-    ...         i += 1
-    ...         raise EC2ResponseError( 'foo', 'bar' )
-    Traceback (most recent call last):
-    ...
-    EC2ResponseError: EC2ResponseError: foo bar
-    <BLANKLINE>
-    >>> i
-    1
-
-    Don't retry on success
-    >>> i = 0
-    >>> for attempt in retry_ec2( retry_after=0, retry_for=.1, retry_while=true ):
-    ...     with attempt:
-    ...         i += 1
-    >>> i
-    1
-
-    Don't retry on unless condition returns
-    >>> i = 0
-    >>> for attempt in retry_ec2( retry_after=0, retry_for=.1, retry_while=false ):
-    ...     with attempt:
-    ...         i += 1
-    ...         raise EC2ResponseError( 'foo', 'bar' )
-    Traceback (most recent call last):
-    ...
-    EC2ResponseError: EC2ResponseError: foo bar
-    <BLANKLINE>
-    >>> i
-    1
-    """
-    if retry_for > 0:
-        go = [ None ]
-
-        @contextmanager
-        def repeated_attempt( ):
-            try:
-                yield
-            except EC2ResponseError as e:
-                if time.time( ) + retry_after < expiration and retry_while( e ):
-                    log.info( '... got %s, trying again in %is ...', e.error_code, retry_after )
-                    time.sleep( retry_after )
-                else:
-                    raise
-            else:
-                go.pop( )
-
-        expiration = time.time( ) + retry_for
-        while go:
-            yield repeated_attempt( )
-    else:
-        @contextmanager
-        def single_attempt( ):
-            yield
-
-        yield single_attempt( )
+def retry_ec2( retry_after=a_short_time, retry_for=10 * a_short_time, retry_while=not_found ):
+    t = retry_after
+    return retry( delays=(t,t,t*2,t*4), timeout=retry_for, predicate=retry_while )
 
 
 class EC2VolumeHelper( object ):
@@ -390,7 +296,7 @@ def wait_spot_requests_active( ec2, requests, timeout=None, tentative=False ):
 
     try:
         while True:
-            open_ids, eval_ids, fulfill_ids = set(), set(), set()
+            open_ids, eval_ids, fulfill_ids = set( ), set( ), set( )
             batch = [ ]
             for r in requests:
                 if r.state == 'open':
@@ -400,8 +306,8 @@ def wait_spot_requests_active( ec2, requests, timeout=None, tentative=False ):
                     elif r.status.code == 'pending-fulfillment':
                         fulfill_ids.add( r.id )
                     else:
-                        log.info('Request %s entered status %s indicating that it will not be '
-                                 'fulfilled anytime soon.', r.id, r.status.code )
+                        log.info( 'Request %s entered status %s indicating that it will not be '
+                                  'fulfilled anytime soon.', r.id, r.status.code )
                 elif r.state == 'active':
                     assert r.id not in active_ids
                     active_ids.add( r.id )
@@ -509,4 +415,3 @@ def tag_object_persistently( tagged_ec2_object, tags_dict ):
     for attempt in retry_ec2( ):
         with attempt:
             tagged_ec2_object.add_tags( tags_dict )
-
