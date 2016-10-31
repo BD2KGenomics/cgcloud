8652f84052966617a2a18edc0116e4654c9a4f8a
Hannes Schmidt <hannes@ucsc.edu>
2016-10-31 12:14:07 -0700

    Bump toil-latest-box to install pre-releases of Toil up to 3.5.0 (fixes #239)

--- toil/src/cgcloud/toil/toil_box.py
+++ toil/src/cgcloud/toil/toil_box.py
@@ -176,7 +176,7 @@ class ToilLatestBox( ToilBox ):
     """
     A box with Mesos, the latest unstable release of Toil and their dependencies installed
     """
-    default_spec = 'toil[aws,mesos,encryption,cwl]<=3.4.0'
+    default_spec = 'toil[aws,mesos,encryption,cwl]<=3.5.0'
 
 
 class ToilLeader( ToilBox, ClusterLeader ):
